97f6733afc41d842789c8592a3d26d3bbdde48fa
Nurzhigit <Nurjigit00a@gmail.com>
2024-07-07 00:11:47 +0500

    There was work with map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,20 +16,36 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
+  let dialogsData = [
+    { id: 1, name: "Dimych" },
+    { id: 2, name: "Andrew" },
+    { id: 3, name: "Sveta" },
+    { id: 4, name: "Sasha" },
+    { id: 5, name: "Nikita" },
+    { id: 6, name: "Kolya" },
+  ];
+
+  let messagesData = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How is it going?" },
+    { id: 3, message: "How old are you?" },
+    { id: 4, message: "Are you rich?" },
+    { id: 5, message: "Yoo" },
+    { id: 6, message: "Goodbye" },
+  ];
+
+  let messagesElements = messagesData.map((m) => (
+    <Message message={m.message} />
+  ));
+
+  let dialogsElements = dialogsData.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogItems}>
-        <DialogItem name="Dimych" id="1" />
-        <DialogItem name="Andrey" id="2" />
-        <DialogItem name="Matvey" id="3" />
-        <DialogItem name="Valeria" id="4" />
-        <DialogItem name="Sveta" id="5" />
-      </div>
-      <div className={s.messages}>
-        <Message message="Hi" />
-        <Message message="How is it going?" />
-        <Message message="How old are you?" />
-      </div>
+      <div className={s.dialogItems}>{dialogsElements}</div>
+      <div className={s.messages}>{messagesElements}</div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,16 @@ import s from "./MyPosts.module.css";
 import ThePost from "./ThePost/Post";
 
 const MyPosts = () => {
+  let posts = [
+    { id: 1, message: "Hi, How are you?", like: 12 },
+    { id: 2, message: "It's my first post", like: 19 },
+    { id: 3, message: "How old are you?", like: 0 },
+  ];
+
+  let postsElements = posts.map((p) => (
+    <ThePost message={p.message} like={p.like} />
+  ));
+
   return (
     <div className={s.postsBlock}>
       <h2>My posts</h2>
@@ -14,10 +24,7 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        <ThePost message="Hi, How are you?" like="0" />
-        <ThePost message="It's my first post" like="18" />
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 };
